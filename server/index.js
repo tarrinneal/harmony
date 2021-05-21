c6f57f5e56da8e4176521465772c6a0453e003ef
tarrinneal <tarrinneal@gmail.com>
2021-05-20 22:26:05 -0700

    basic html/css and socket io set up

--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
