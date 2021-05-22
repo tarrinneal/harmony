ff81299248de2a8b3c35eb9ff2ae224f911bedd9
tarrinneal <tarrinneal@gmail.com>
2021-05-21 22:50:43 -0700

    oops, I deleted everything

--- client/src/components/LeftSidebar.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const LeftSidebar = () => (
-  <div id='leftSidebar'>
-    <h2 id='featureNavigation'>Servers</h2>
-  </div>
-);
--- client/src/components/RightSidebar.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const RightSidebar = () => (
-  <div id='rightSidebar'>
-    <h2 id='featureNavigation'>Online Users</h2>
-  </div>
-);
