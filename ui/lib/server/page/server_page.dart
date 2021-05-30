ac945e3c68ad037ad151d55216c89a9ab0be8956
tarrinneal <tarrinneal@gmail.com>
2021-05-29 20:36:01 -0700

    fixed sizing in listview for messages

--- ui/lib/server/page/server_page.dart
+++ ui/lib/server/page/server_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import '../widget/message.dart';
 
+typedef User = String;
+
+class MessageData {
+  const MessageData({
+    required this.user,
+    required this.timestamp,
+    required this.message
+  });
+
+  final User user;
+  final DateTime timestamp;
+  final String message;
+}
+
 final messages = [
-  for (var i = 1; i < 15; i++)
-    {
-      'user': 'user $i',
-      'timestamp': DateTime.now().toString(),
-      'message': 'This is a message, wow.'
-    }
+  for (var i = 1; i < 150; i++)
+    MessageData(user: 'user $i', timestamp: DateTime.now(), message: 'This is a message, wow.',)
 ];
 
 class ServerPage extends StatelessWidget {
@@ -16,14 +26,12 @@ class ServerPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         child: ListView.builder(
-            itemCount: messages.length,
-            itemBuilder: (BuildContext context, int i) {
-              return Container(
-                //needs to be dynamically sized
-                  height: 100,
-                  width: 1000,
-                  child: Message(message: messages[i]));
-            }))
-      );
+          itemCount: messages.length,
+          itemBuilder: (BuildContext context, int i) {
+            return Message(message: messages[i]);
+          },
+        ),
+      ),
+    );
   }
 }
