f19011e5d3734788558e3fe8c337f8d669954069
tarrinneal <tarrinneal@gmail.com>
2021-05-31 16:54:40 -0700

    added user status column with dummy data, made main server page resizable

--- ui/lib/server/page/server_page.dart
+++ ui/lib/server/page/server_page.dart
@@ -1,36 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:harmony/spec/spec.dart';
 import '../widget/message.dart';
+import '../widget/user.dart';
+import 'dart:math';
 
 typedef User = String;
 
 class MessageData {
-  const MessageData({
-    required this.user,
-    required this.timestamp,
-    required this.message
-  });
+  const MessageData(
+      {required this.user, required this.timestamp, required this.message});
 
   final User user;
   final DateTime timestamp;
   final String message;
 }
 
+class UserData {
+  const UserData({
+    required this.user,
+    required this.status,
+  });
+
+  final User user;
+  final bool status;
+}
+
 final messages = [
   for (var i = 1; i < 150; i++)
-    MessageData(user: 'user $i', timestamp: DateTime.now(), message: 'This is a message, wow.',)
+    MessageData(
+      user: 'user $i',
+      timestamp: DateTime.now(),
+      message: 'This is a message, wow.',
+    )
+];
+
+final users = [
+  for (var i = 1; i < 150; i++)
+    UserData(user: 'user $i', status: Random().nextInt(100) > 50)
 ];
 
 class ServerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: ListView.builder(
-          itemCount: messages.length,
-          itemBuilder: (BuildContext context, int i) {
-            return Message(message: messages[i]);
-          },
-        ),
+      body: Row(
+        children: [
+          Flexible(
+            child: Container(
+              child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (BuildContext context, int i) {
+                return Message(message: messages[i]);
+              },
+              ),
+            ),
+          ),
+          Container(
+            width: Sizing.homeSideBarWidth,
+            child: ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (BuildContext context, int j) {
+              return UserStatus(user: users[j]);
+            },
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ ui/lib/server/widget/user.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:harmony/server/server.dart';
+
+class UserStatus extends StatelessWidget {
+  const UserStatus({required this.user});
+
+  final UserData user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(user.user),
+          Text(user.status ? 'Online' : 'Offline')
+        ],
+      ),
+    );
+  }
+}
