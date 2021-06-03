20c31e17f0acfb4602f632a2a7f713c6523e1261
tarrinneal <tarrinneal@gmail.com>
2021-06-02 22:32:26 -0700

    changed listview to listview.builder with a large refactor of sidebar to allow for home button

--- ui/lib/app_shell/widget/server_button.dart
+++ ui/lib/app_shell/widget/server_button.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:harmony/spec/spec.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../roundedHexagonalClipper.dart';
+
+final String assetName = './Harmony.svg';
 
 class ServerButton extends StatelessWidget {
   const ServerButton({
     required this.focused,
     required this.label,
     required this.onPressed,
+    required this.i,
     this.iconData,
   });
 
   final String label;
-  final IconData? iconData;
   final bool focused;
   final void Function(String) onPressed;
+  final int i;
+  final IconData? iconData;
 
   @override
   Widget build(BuildContext context) {
+    final logoSvg = SvgPicture.asset(
+      assetName,
+      semanticsLabel: 'Harmony Logo',
+      fit: BoxFit.scaleDown,
+      height: Sizing.serverSideBarWidth,
+    );
     return SizedBox.fromSize(
       size: Size.square(Sizing.serverSideBarWidth),
-      child: Container(
+      child: i == 0 ? Container(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: focused
+                    ? Theme.of(context).backgroundColor
+                    : Theme.of(context).primaryColorLight,
+              ),
+              onPressed: () {
+                onPressed('');
+              },
+              child: ClipPath(
+                clipper: RoundedHexagonalClipper(),
+                child: logoSvg,
+              ),
+            ),
+          ) : Container(
         child: TextButton(
           onPressed: () {
             onPressed(label);
@@ -36,16 +62,8 @@ class ServerButton extends StatelessWidget {
           ),
         ),
       ),
-
-      // TextButton.icon(
-      //   icon: Icon(iconData ?? Icons.person_outline),
-      //   onPressed: () => onPressed(label),
-      //   label: Text(label),
-      //   style: TextButton.styleFrom(
-      //     backgroundColor: focused
-      //         ? Theme.of(context).backgroundColor
-      //         : Theme.of(context).primaryColorLight,
-      // ),
     );
   }
 }
+
+
--- ui/lib/app_shell/widget/server_sidebar.dart
+++ ui/lib/app_shell/widget/server_sidebar.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:harmony/spec/spec.dart';
 
 import './server_button.dart';
-import '../../roundedHexagonalClipper.dart';
 
 final serverLabels = [
   for (var i = 1; i < 11; i++) 'Server $i',
 ];
-
-final String assetName = './Harmony.svg';
 
 class ServerSidebar extends StatefulWidget {
   @override
@@ -27,46 +23,27 @@ class _ServerSidebarState extends State<ServerSidebar> {
 
   @override
   Widget build(BuildContext context) {
-    final logoSvg = SvgPicture.asset(
-      assetName,
-      semanticsLabel: 'Harmony Logo',
-      fit: BoxFit.scaleDown,
-      height: Sizing.serverSideBarWidth,
-    );
+
     return SizedBox.fromSize(
       size: Size.fromWidth(Sizing.serverSideBarWidth),
-      child: ListView(
-        children: [
-          Container(
-            child: TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: selected == ''
-                    ? Theme.of(context).backgroundColor
-                    : Theme.of(context).primaryColorLight,
-              ),
-              onPressed: () {
-                select('');
-              },
-              child: ClipPath(
-                clipper: RoundedHexagonalClipper(),
-                child: logoSvg,
-              ),
-            ),
-          ),
-          for (final server in serverLabels)
+      child: ListView.builder(
+        itemCount: serverLabels.length + 1,
+        itemBuilder: (context, i) =>
             Column(
               children: [
                 SizedBox(height: 10),
                 ServerButton(
-                  label: server,
+                  label: i == 0 ? '' : serverLabels[i - 1],
                   iconData: Icons.games_outlined,
-                  focused: server == selected,
+                  focused: i == 0 ? selected == '' : serverLabels[i - 1] == selected,
                   onPressed: select,
+                  i: i,
                 ),
               ],
             )
-        ],
+
       ),
     );
   }
 }
+
