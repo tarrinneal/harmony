fd76214c24ccf95149ce99008a8948ea7edc4fa5
atypicaldev <atypicaldev@gmail.com>
2021-06-03 11:38:59 -0400

    Refactor app root router based navigation

--- ui/lib/app.dart
+++ ui/lib/app.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:harmony/app_shell/app_shell.dart';
+// import 'package:harmony/app_shell/app_shell.dart';
+import 'package:harmony/routing/routing.dart';
 
 class HarmonyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationParser: HarmonyRouteInformationParser(),
+      routerDelegate: HarmonyRouterDelegate(
+        AppRouteState(
+          authenticated: false,
+        ),
+      ),
       title: 'Harmony',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
         primaryColor: Colors.blueGrey[900],
       ),
-      home: AppShell(),
     );
   }
 }
--- ui/lib/routing/routing.dart
+++ ui/lib/routing/routing.dart
@@ -1,1 +1,3 @@
-export 'harmony_navigator.dart';
+export 'config/harmony_route_information_parser.dart';
+export 'config/app_route_state.dart';
+export 'widget/harmony_router_delegate.dart';
