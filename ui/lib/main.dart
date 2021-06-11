ddb56d0a38f48ad61799dab027b5edc920c2a05f
GikuyuNderitu <gikuyunderitu@gmail.com>
2021-06-11 15:59:22 -0400

    Provide Authservice implementation throughout the app

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:harmony/auth/auth.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(HarmonyApp());
+  runApp(
+    Provider<AuthService>(
+      create: (context) => AuthService.fake(),
+      child: HarmonyApp(),
+    ),
+  );
 }
