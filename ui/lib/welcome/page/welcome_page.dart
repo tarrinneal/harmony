3eaea531e5fe405d6bed283e873ffa6805cce938
GikuyuNderitu <gikuyunderitu@gmail.com>
2021-06-12 18:19:48 -0400

    Add AppBar to Welcome page

--- ui/lib/welcome/page/welcome_page.dart
+++ ui/lib/welcome/page/welcome_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:harmony/routing/widget/harmony_router_delegate.dart';
 import 'package:harmony/state/state.dart';
 
+import '../widget/welcome_app_bar.dart';
+
 /// Displays the screen that the user views when they first visit the app
 /// and they aren't authenticated.
 class WelcomePage extends StatelessWidget {
@@ -11,6 +13,17 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final appState = AppRouteScope.of(context);
     return Scaffold(
+      appBar: WelcomeAppBar(
+        title: 'Harmony',
+        actions: [
+          ActionButton(
+            title: 'Login',
+            onPressed: () {
+              print('go to login page');
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
