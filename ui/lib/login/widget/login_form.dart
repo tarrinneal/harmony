10eca78d4c1314ce0383c3708a1ecf7d76d45d55
tarrinneal <tarrinneal@gmail.com>
2021-06-15 00:12:49 -0700

    form complete besides auth

--- ui/lib/login/widget/login_form.dart
+++ ui/lib/login/widget/login_form.dart
@@ -39,12 +39,8 @@ class LoginFormState extends State<LoginForm> {
             ),
             ElevatedButton(
               onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
                 if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  //auth
                 }
               },
               child: Text('Submit'),
