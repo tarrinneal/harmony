df346642411028dd2f005d2c3357e4497cbf505d
tarrinneal <tarrinneal@gmail.com>
2021-06-14 23:53:49 -0700

    login page halfway set up

---
+++ ui/lib/login/page/login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:harmony/spec/spec.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widget/login_form.dart';
+
+final String assetName = './Harmony.svg';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          SvgPicture.asset(
+            assetName,
+            semanticsLabel: 'Harmony Logo',
+            fit: BoxFit.scaleDown,
+            height: Sizing.serverSideBarWidth,
+          ),
+          Container(
+            child: LoginForm(label: label),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ ui/lib/login/widget/login_form.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    required this.label,
+    this.iconData,
+  });
+
+  final String label;
+  final IconData? iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        alignment: Alignment.center,
+        fit: BoxFit.contain,
+        child: Form(child: Column(children: <Widget>[
+          TextFormField(validator: (String? value) {
+            return (RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$').hasMatch(value) ? null : 'Not a valid email')
+          },)
+        ],)),
+      ),
+    );
+  }
+}
