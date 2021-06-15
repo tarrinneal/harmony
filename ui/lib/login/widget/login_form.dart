3098fbcd56640c2041b461d791d4422e9b48ea03
tarrinneal <tarrinneal@gmail.com>
2021-06-15 00:11:00 -0700

    added state to form

--- ui/lib/login/widget/login_form.dart
+++ ui/lib/login/widget/login_form.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LoginForm extends StatelessWidget {
-  const LoginForm({
-    required this.label,
-    this.iconData,
-  });
+class LoginForm extends StatefulWidget {
+  @override
+  LoginFormState createState() {
+    return LoginFormState();
+  }
+}
 
-  final String label;
-  final IconData? iconData;
+class LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,28 @@ class LoginForm extends StatelessWidget {
                     ? null
                     : 'Not a valid email');
               },
-            )
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (String? value) {
+                return (value != null && value.length > 8
+                    ? null
+                    : 'Password must be 8 characters or longer');
+              },
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
           ],
         )),
       ),
