e10827e62953ae8f31b1ada698a3fb667c1e3c3f
tarrinneal <tarrinneal@gmail.com>
2021-06-14 23:56:41 -0700

    finished email validator

--- ui/lib/login/widget/login_form.dart
+++ ui/lib/login/widget/login_form.dart
@@ -15,11 +15,20 @@ class LoginForm extends StatelessWidget {
       child: FittedBox(
         alignment: Alignment.center,
         fit: BoxFit.contain,
-        child: Form(child: Column(children: <Widget>[
-          TextFormField(validator: (String? value) {
-            return (RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$').hasMatch(value) ? null : 'Not a valid email')
-          },)
-        ],)),
+        child: Form(
+            child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (String? value) {
+                return (value != null &&
+                        RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                            .hasMatch(value)
+                    ? null
+                    : 'Not a valid email');
+              },
+            )
+          ],
+        )),
       ),
     );
   }
