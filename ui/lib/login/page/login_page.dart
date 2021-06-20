19c586778408c567a21c0deb29c4927605fa2365
tarrinneal <tarrinneal@gmail.com>
2021-06-20 12:52:08 -0700

    moved scaffold to login page to increase modularity

--- ui/lib/login/page/login_page.dart
+++ ui/lib/login/page/login_page.dart
@@ -10,22 +10,24 @@ final String assetName = './Harmony.svg';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      heightFactor: 1,
-      widthFactor: 1,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          SvgPicture.asset(
-            assetName,
-            semanticsLabel: 'Harmony Logo',
-            fit: BoxFit.fill,
-          ),
-          LoginForm(
-            onSubmit: () =>
-                {AppRouteScope.of(context).selectedPage = HarmonyPage.server},
-          ),
-        ],
+    return Scaffold(
+      body: FractionallySizedBox(
+        heightFactor: 1,
+        widthFactor: 1,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              assetName,
+              semanticsLabel: 'Harmony Logo',
+              fit: BoxFit.fill,
+            ),
+            LoginForm(
+              onSubmit: () =>
+                  {AppRouteScope.of(context).selectedPage = HarmonyPage.server},
+            ),
+          ],
+        ),
       ),
     );
   }
