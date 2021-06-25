9960440864e587798a9c563e457fd73ee9340c39
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2021-06-25 03:30:18 -0700

    Add accessToken to User definition

--- ui/lib/auth/models/user.dart
+++ ui/lib/auth/models/user.dart
@@ -1,7 +1,21 @@
 /// Defines the model that encapsualtes a user in the auth context.
+
+const _EMPTY_TOKEN = '';
+
 class User {
-  const User({required this.username, required this.email});
+  const User({
+    required this.username,
+    required this.email,
+    this.token = _EMPTY_TOKEN,
+  });
 
   final String username;
   final String email;
+  final String token;
+
+  User addToken(String token) {
+    return User(username: username, email: email, token: token);
+  }
+
+  bool get hasToken => token != _EMPTY_TOKEN;
 }
