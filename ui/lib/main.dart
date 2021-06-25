1f7df8acd66bb780a7fd1f3ab50c4f34fb3750f8
Gikuyu Nderitu <gikuyunderitu@gmail.com>
2021-06-25 02:31:07 -0700

    Setup Firebase

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:harmony/auth/auth.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(
     Provider<AuthService>(
       create: (context) => AuthService.fake(),
