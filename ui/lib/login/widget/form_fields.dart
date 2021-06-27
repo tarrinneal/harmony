fef83fb3cd907c04cffa26b0f9b0545ad107f2ba
tarrinneal <tarrinneal@gmail.com>
2021-06-27 02:16:59 -0700

    refactor FormField to stateless widget

--- ui/lib/login/widget/form_fields.dart
+++ ui/lib/login/widget/form_fields.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 
-class FormField extends StatefulWidget {
+class FormField extends StatelessWidget {
   const FormField({required this.name, required this.validator});
 
   final String name;
   final String? Function(String?) validator;
 
   @override
-  FormFieldState createState() => FormFieldState();
-}
-
-class FormFieldState extends State<FormField> {
-  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      validator: widget.validator,
-      decoration: InputDecoration(labelText: widget.name),
+      validator: validator,
+      decoration: InputDecoration(labelText: name),
     );
   }
 }
